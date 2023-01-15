f610c52c58a290590cb9202a6cc88a3fe57313c7
scrunchygamergirl <nataniahwang1010@gmai.com>
2023-01-15 15:09:00 -0500

    link main.js within index.html

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const totalPoints = document.querySelector('.score')
+const holes = document.querySelectorAll('.hole')
+const moles = document.querySelectorAll('.mole')
+console.log(totalPoints)
+console.log(holes)
+console.log(moles)
