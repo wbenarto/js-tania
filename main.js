ab867e33f1970466b919fa5317a36fde1380765c
w.benarto <w.benarto@gmail.com>
2023-01-29 12:29:22 -0800

    added random functions for nums

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ const moles = document.querySelectorAll('.mole')
 console.log(totalPoints)
 console.log(holes)
 console.log(moles)
+
+function randomTime(min, max) {
+    // return us a randomized time for the moles to show up
+    // within the min, and max values given
+    return Math.round(Math.random() * (max-min) + min)
+}
+
+function randomHole(holes) {
+    let idx = Math.floor(Math.random() * holes.length)
+    const hole = holes[idx]
+    console.log(hole)
+    return idx
+}
+
+// randomize time between 15 sec to 200secs
+randomTime(15, 200)
+
+console.log('Random function returns ', randomTime(10, 100))
+console.log('RAndomized index , ' + randomHole(holes))
