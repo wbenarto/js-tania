6d80d07bcfd587538dc2ea46175f82dad81c2204
w.benarto <w.benarto@gmail.com>
2023-02-12 13:02:18 -0800

    javascript functions, mole properties, todo for tania

--- README.md
+++ README.md
@@ -13,3 +13,18 @@ Start Game
 Mole to hide and show, within the time set
 OnClick function when mole is hit
 Random Pop up for mole
+
+
+## To Do for Tania
+### 2/12/2023
+### 2 hours Reviewing HTML and CSS
+### Properties on CSS, HTML setup 
+### log the time you work on 
+
+### 2/19/2023
+### Finish Javascript Functions and game
+### improve design 
+
+
+### 2/26/2023
+### Tania Test 
--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 
-const totalPoints = document.querySelector('.score')
+
+let scoreboard = document.querySelector('.score')
 const holes = document.querySelectorAll('.hole')
 const moles = document.querySelectorAll('.mole')
-console.log(totalPoints)
+console.log(scoreboard)
 console.log(holes)
 console.log(moles)
+let lastHole;
+let timeUp = false;
 
 function randomTime(min, max) {
     // return us a randomized time for the moles to show up
@@ -15,12 +18,32 @@ function randomTime(min, max) {
 function randomHole(holes) {
     let idx = Math.floor(Math.random() * holes.length)
     const hole = holes[idx]
-    console.log(hole)
-    return idx
+
+    if (hole == lastHole) {
+       return randomHole(holes)
+    }
+
+    lastHole = hole
+
+    return hole
 }
 
-// randomize time between 15 sec to 200secs
-randomTime(15, 200)
+function peep() {
+    const time = randomTime(200, 1000)
+    const hole = randomHole(holes)
+    hole.classList.add("up")
+    setTimeout(()=>{
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+    }, time)
+}
 
-console.log('Random function returns ', randomTime(10, 100))
-console.log('RAndomized index , ' + randomHole(holes))
+function startGame() {
+    console.log('game started')
+    scoreboard= 0;
+    timeUp = false;
+    peep();
+    setTimeout(()=> timeUp = true, 10000)
+    
+}
+// randomize time between 15 sec to 200secs
