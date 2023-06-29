af8695f8db7efbfed65eb615f4300fb94db46868
william benarto <w.benarto@gmail.com>
2023-06-29 10:26:11 -0700

    fundamentals are important yall

---
+++ Fundamentals/objects.js
@@ -0,0 +1,21 @@
+// Data Structure fundamentals
+
+// Data structure is a way of keeping data in our programs
+// Strings "Pineapple"
+// Arrays ["apple", 1, true, false, "kidding"] 
+// Object - Hash Maps 
+
+// Object is always paired with Keys and Values
+// Cars is variable
+// Keys and Values are below
+let cars = {
+    color: "pink",
+    brand: "Mercedes Benz",
+    year: 2016,
+}
+
+console.log(cars)
+// Dot Notation 
+console.log(cars.color)
+// Bracket Notation 
+console.log(cars["color"])
